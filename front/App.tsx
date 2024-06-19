62d6e69bd5e791a1fc063b26d08328cb46b02682
whitej00 <68831140+whitej00@users.noreply.github.com>
2024-06-19 22:35:03 +0900

    first commit

---
+++ front/App.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import AuthStackNavigator from './src/navigations/stack/AuthStackNavigator';
+import RootNavigator from './src/navigations/root/RootNavigator';
+
+function App(): JSX.Element {
+
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
